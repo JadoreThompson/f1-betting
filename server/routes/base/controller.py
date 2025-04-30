a6e000ff302ce271e30db72789f29bf1b6089894
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2025-05-01 00:26:48 +0100

    removed comments from routes/base/controller

--- server/routes/base/controller.py
+++ server/routes/base/controller.py
@@ -4,40 +4,6 @@ from engine import interact, Prediction
 from server.utils.db import get_db_session
 
 
-# BASE_PATH = "https://ergast.com/api/f1"
-# NS = {"mrd": "http://ergast.com/mrd/1.5"}
-
-
-# def parse_standings(xml_string: str) -> Tuple[list[Driver], str]:
-#     root = ET.fromstring(xml_string)
-#     results: list[Driver] = []
-
-#     for result in root.findall(".//mrd:Driver", NS):
-#         results.append(
-#             Driver(
-#                 name=result.attrib["driverId"],
-#                 number=result.findtext("mrd:PermanentNumber", namespaces=NS),
-#             )
-#         )
-
-#     return results, root.find(".//mrd:StandingsList", NS).attrib["round"]
-
-
-# async def fetch_standings(
-#     year: int | None = None, round_: int | None = None
-# ) -> Tuple[list[Driver], str]:
-#     if year is None:
-#         year = datetime.now().year
-
-#     async with AsyncClient() as client:
-#         rsp = await client.get(
-#             BASE_PATH
-#             + f"/{year}{f"/{round_}" if round_ is not None else ""}/driverStandings"
-#         )
-#         if rsp.status_code != 200:
-#             raise ValueError(f"Request failed with status code: {rsp.status_code}")
-#         return parse_standings(rsp.text)
-
 
 async def get_predictions() -> tuple[int, str, str, float]:
 
