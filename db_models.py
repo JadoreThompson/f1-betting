c128dd0ce936e915f2ee94a24df6442d692f9ff2
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2025-05-31 11:01:20 +0100

    Created users, markets and bets table

--- db_models.py
+++ db_models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import ForeignKey, Integer, String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql import text
+from sqlalchemy import UUID, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from betting_engine.enums import BetStatus, Side
 
 
 class Base(DeclarativeBase):
@@ -28,3 +30,57 @@ class F1Data(Base):
     last_3: Mapped[str] = mapped_column(String, nullable=True)
     last_4: Mapped[str] = mapped_column(String, nullable=True)
     last_5: Mapped[str] = mapped_column(String, nullable=True)
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    user_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
+    username: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+
+    user_bets = relationship(
+        "Bets", back_populates="user", cascade="all, delete-orphan"
+    )
+
+
+class Markets(Base):
+    """Represents a market for betting."""
+
+    __tablename__ = "markets"
+
+    market_id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    numerator: Mapped[int] = mapped_column(Integer, nullable=False)
+    denominator: Mapped[int] = mapped_column(Integer, nullable=False)
+
+
+class Bets(Base):
+    """Represents a bet placed by a user on a market."""
+
+    __tablename__ = "bets"
+
+    bet_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("users.user_id"), nullable=False
+    )
+    market_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("markets.market_id"), nullable=False
+    )
+    side: Mapped[str] = mapped_column(String, nullable=False)
+    amount: Mapped[int] = mapped_column(Integer, nullable=False)
+    wallet_address: Mapped[str] = mapped_column(String, nullable=False)
+    bet_status: Mapped[BetStatus] = mapped_column(
+        String(20), nullable=False, default=BetStatus.PENDING.value
+    )
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+    closed_at: Mapped[DateTime] = mapped_column(
+        DateTime, nullable=True, server_default=None
+    )
+
+    user = relationship("User", back_populates="user_bets")
